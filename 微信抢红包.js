cffb28e17dea85b37fc8880506a3dd7e2ae79710
Xiongjie Xue <xxj95719@gmail.com>
2022-03-03 10:56:17 +0800

    Create 微信抢红包.js
    初版本微信红包脚本

---
+++ 微信抢红包.js
@@ -0,0 +1,24 @@
+auto();
+toastLog("准备抢红包，请将屏幕保持在微信的聊天窗口")
+device.keepScreenOn()
+
+while (true) {
+  id('com.tencent.mm:id/ape').find().forEach((item, index) => {
+    if (item.find(text('已领取')).size() > 0) {
+      log('第' + index + '个红包已领取')
+    } else {
+      log("发现新红包");
+      click(item.bounds().centerX(), item.bounds().centerY())
+      const draw = desc("开").findOne(500);
+      if (draw != null) {
+        log("点开新红包");
+        click(draw.bounds().centerX(), draw.bounds().centerY())
+        sleep(1000);
+        back();
+      } else {
+        log("过期之类无效红包");
+      }
+    }
+  })
+  sleep(1000)
+}
