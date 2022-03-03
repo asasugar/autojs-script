be7e5ebd9979eb8a2755ad29676aa22991811923
Xiongjie Xue <xxj95719@gmail.com>
2022-03-03 14:52:03 +0800

    Update 微信抢红包.js

--- 微信抢红包.js
+++ 微信抢红包.js
@@ -1,18 +1,19 @@
-auto();
-toastLog("准备抢红包，请将屏幕保持在微信的聊天窗口")
-device.keepScreenOn()
+waitFor();
+launchApp('微信');
+toastLog("准备抢红包，请将屏幕保持在微信的聊天窗口");
+device.keepScreenOn();
 
 while (true) {
   id('com.tencent.mm:id/ape').find().forEach((item, index) => {
     if (item.find(text('已领取')).size() > 0) {
-      log('第' + index + '个红包已领取')
+      log('第' + index + '个红包已领取');
     } else {
       log("发现新红包");
-      click(item.bounds().centerX(), item.bounds().centerY())
+      click(item.bounds().centerX(), item.bounds().centerY());
       const draw = desc("开").findOne(500);
       if (draw != null) {
         log("点开新红包");
-        click(draw.bounds().centerX(), draw.bounds().centerY())
+        click(draw.bounds().centerX(), draw.bounds().centerY());
         sleep(1000);
         back();
       } else {
@@ -20,5 +21,5 @@ while (true) {
       }
     }
   })
-  sleep(1000)
+  sleep(1000);
 }
