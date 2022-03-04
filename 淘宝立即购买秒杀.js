187265b9ad784403ed9d53ec3e1c2b9fe7cfe506
Xiongjie Xue <xxj95719@gmail.com>
2022-03-04 13:45:15 +0800

    Update 淘宝立即购买秒杀.js

--- 淘宝立即购买秒杀.js
+++ 淘宝立即购买秒杀.js
@@ -4,9 +4,14 @@ toastLog("准备淘宝秒杀");
 device.keepScreenOn();
 while (true) {
   var button = text('立即购买').findOne(400);
+  var couponButton = text('领券购买').findOne(400);
   if (button != null) {
-    log("找到购买按钮");
+    log("找到“立即购买”按钮");
     click(button.bounds().left, button.bounds().bottom);
+    sleep(1000);
+  } else if(couponButton != null) {
+    log("找到“领券购买”按钮");
+    click(couponButton.bounds().left, couponButton.bounds().bottom);
     sleep(1000);
   } else {
     log("按钮不存在");
